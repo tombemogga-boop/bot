02eef60e8d79e52cb8330bff325d52c5f3c1aa99
tombemogga-boop <tombemoggagabriel@gmail.com>
2026-04-28 00:46:55 +0300

    NEW# This is a combination of 2 commits.

--- main.py
+++ main.py
@@ -65,4 +65,5 @@ def webhook():
 
 # Run app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
